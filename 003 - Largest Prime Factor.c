ae7dde22a9bd598a55972fa88542059131ae8eff
ramonigl <143233276+ramonigl@users.noreply.github.com>
2025-03-11 16:14:01 -0300

    Improve 003 - Largest Prime Factor.c
    Correcting some problems:
    
    1. isPrime was being initialized incorrectly
    
    2. the factoring logic was improved:
    the factoring of a prime number do not need to be tested up to itself, we just need to test up to the square root of the number.
    
    3. prime was being initialized as 1, but the factoring should star at 3 since we already tested 2 separately.
    
    4. when a prime number was found, num was divided just once and the iteration kept going. But one prime can divid num multiple times.

--- 003 - Largest Prime Factor.c
+++ 003 - Largest Prime Factor.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
 #include <math.h>
 
-
 #define NUM 600851475143
 
 int main(){
 
-    int prime = 1, isPrime = 0;
+    int prime = 2, isPrime = 1;
     long num = NUM;
 
     printf("\nnum = %ld\n", num);
 
-    while (num % 2 == 0) {
+    while (num % prime == 0) {
         num /= 2;
     }
 
-    for (prime = 1; prime < num; prime+=2){
+    for (prime = 3; prime < sqrt(num); prime+=2){
         for (int i = 3; i < prime; i+=2) {
             if (prime % i == 0) {
                 isPrime = 0;
                 break;
             }
         }
-        if (isPrime && (num % prime == 0)) {
+        while (isPrime && (num % prime == 0)) {
             num = num / prime;
         }
 
         isPrime = 1;
     }
+    if (num > 1)
+        prime = num;
 
-    printf("prime = %d\n\n", prime);
+    printf("largest prime = %d\n\n", prime);
 
-    return prime;
+    return 0;
 }
