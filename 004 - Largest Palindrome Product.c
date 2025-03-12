247d3bb9ab880f601bb428fbf286191b508b803c
ramonigl <143233276+ramonigl@users.noreply.github.com>
2025-03-12 16:53:52 -0300

    Improving 004 - Largest Palindrome Product.c
    breaks the inner loop after finding the largest palindrome, since j is decreasing no larger palindrome can be found with the same i.

--- 004 - Largest Palindrome Product.c
+++ 004 - Largest Palindrome Product.c
@@ -14,17 +14,16 @@ int isPalindrome(int num) {
 
 int main(){
 
-    int largestPalindrome;
+    int largestPalindrome = 0;
 
     for (int i = 999; i >= 100; i--){
         for (int j = i; j >= 100; j--){
 
             int num = j * i;
         
-            if (isPalindrome(num)){        
-                if (num > largestPalindrome) {
-                    largestPalindrome = num;
-                }
+            if (isPalindrome(num) && num > largestPalindrome){        
+                largestPalindrome = num;
+                break;
             }  
         }
     }
